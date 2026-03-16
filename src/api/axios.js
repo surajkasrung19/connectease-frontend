e80d79c5df33960f6e3cf070afa2ea143fb81d6c
Suraj <surajkasrung231@gmail.com>
2026-03-16 20:49:54 +0530

    update axios baseURL for production
    Update base URL to use environment variable for API URL.

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000", //backend URL
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000", //backend URL
 });
 //     Add token automatically
 API.interceptors.request.use((config) => {
